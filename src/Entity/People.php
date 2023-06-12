26d3e313f3ec4c15448af358fe2e2690f633a145
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2023-06-12 12:05:47 +0200

    Creation et ajout de la classe People.php

---
+++ src/Entity/People.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class People
+{
+    private int $id;
+    private string $birthday;
+    private string $deathday;
+    private string $name;
+    private string $longtext;
+    private string $placeOfBirth;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBirthday(): string
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeathday(): string
+    {
+        return $this->deathday;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLongtext(): string
+    {
+        return $this->longtext;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlaceOfBirth(): string
+    {
+        return $this->placeOfBirth;
+    }
+
+}
