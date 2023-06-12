8935fd5890d5302ab35433dea6d4733319913f07
Simon Baptiste <baptiste.simon@etudiant.univ-reims.fr>
2023-06-12 15:31:53 +0200

    ajout des images dans le fichier index grâce au fichier image

--- public/index.php
+++ public/index.php
@@ -4,21 +4,30 @@ declare(strict_types=1);
 
 use Html\AppWebPage;
 use Entity\Collection\MovieCollection;
+use Entity\Collection\ImageCollection;
+use Entity\Collection\PeopleCollection;
 
 
 $webpage = new AppWebPage();
 $webpage->setTitle("Films");
 
-
+$imageCollection = new ImageCollection();
 $movieCollection = new MovieCollection();
 $movies = $movieCollection->findAll();
 
 $content = "<div class='content'>";
 $content .= "<ul class='list'>";
+
+
 foreach ($movies as $movie) {
-    $movieName = $webpage->escapeString($movie->getOriginalTitle());
+    $movieName = $webpage->escapeString($movie->getTitle());
     $content .= "<li class='list-item'>{$movieName}</li>";
+
+    $image = $imageCollection->findById($movie->getPosterId());
+    $content.= "<img src='image.php?imageId={$image->getId()}'>";
 }
+
+
 $content .= "</ul>";
 $content .= "</div>";
 
