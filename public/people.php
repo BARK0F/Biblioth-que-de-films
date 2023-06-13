ee72cba40f1a2d0af4139da1e552a00ddc1efd2b
Simon Baptiste <baptiste.simon@etudiant.univ-reims.fr>
2023-06-13 10:30:06 +0200

    ajout du menu déroulent dans le fichier people relier au menu principal et à la page d'édition

--- public/people.php
+++ public/people.php
@@ -39,9 +39,51 @@ $movieCollection = new MovieCollection();
 $imageCollection = new ImageCollection();
 $CastCollection = new CastCollection();
 
+
+$content = "
+<div class='dropdown'>
+    <button class='dropbtn'>redirection</button>
+    <div class='dropdown-content'>
+      <a href='index.php'>Menu principal</a>
+      <a href='form.php'>Menu d'édition</a>
+    </div>
+  </div>
+";
+
+$webpage->appendCss("
+.dropdown {
+      position: relative;
+      display: inline-block;
+    }
+    
+    .dropdown-content {
+      display: none;
+      position: absolute;
+      background-color: #f9f9f9;
+      min-width: 120px;
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      z-index: 1;
+    }
+    
+    .dropdown:hover .dropdown-content {
+      display: block;
+    }
+    
+    .dropdown-content a {
+      color: black;
+      padding: 12px 16px;
+      text-decoration: none;
+      display: block;
+    }
+    
+    .dropdown-content a:hover {
+      background-color: #f1f1f1;
+    }
+");
+
 $actor = $PeopleCollection->findById(intval($peopleId));
 
-$content = "<div class='principal_content'>";
+$content .= "<div class='principal_content'>";
 if ($actor->getAvatarId() !== null){
     $content .="<img src='image.php?imageId={$actor->getAvatarId()}'>";
 }else{
